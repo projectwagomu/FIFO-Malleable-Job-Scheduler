04f90787d28e4772868f3649899a7c31c635bdf5
Patrick Finnerty <finnerty.patrick@fine.cs.kobe-u.ac.jp>
2023-03-28 11:56:41 +0900

    Added the third-party programs used to direct malleable programs

---
+++ middlelayer/shrink_expand/apgas/Client.java
@@ -0,0 +1,36 @@
+import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.Scanner;
+import java.net.ConnectException;
+
+public class Client {
+  public static void main(String[] args) {
+    // サーバーソケットに接続できるまで試す
+    while (true) {
+      try (Socket socket = new Socket("localhost", 8080);
+          PrintWriter writer = new PrintWriter(socket.getOutputStream(), true)) {
+        String str = new String();
+        for (int i = 0; i < args.length; i++) {
+          str += (args[i] + " ");
+        }
+        writer.println(str);
+        if (args[0].equals("shrink")) {
+          BufferedReader reader;
+          reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+          for (int i = 0; i < Integer.parseInt(args[1]); i++) {
+            String line = reader.readLine();
+            System.out.println(line);
+          }
+        }
+        return;
+      } catch (ConnectException e) {
+        // 接続できなかったらやり直し
+        continue;
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+  }
+}
