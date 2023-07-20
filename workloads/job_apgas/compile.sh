7771a37c9baff0d306a7355c67c40c6f893fed2e
Patrick Finnerty <finnerty.patrick@fine.cs.kobe-u.ac.jp>
2023-07-20 14:47:48 +0900

    Updated script to automatically clone submodule

--- workloads/job_apgas/compile.sh
+++ workloads/job_apgas/compile.sh
@@ -1,8 +1,12 @@
 # Compiling the malleable APGAS library and the lifeline-based global load balancer
 cd apgas
+git submodule init
+git submodule update
 git checkout v0.0.1
 mvn install -DskipTests
 cd ../lifelineglb
+git submodule init
+git submodule update
 git checkout v0.0.1
 mvn package
 
