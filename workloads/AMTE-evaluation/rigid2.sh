bed58ea15a315cb5de81babbeb3fa42349bebce4
Patrick Finnerty <finnerty.patrick@fine.cs.kobe-u.ac.jp>
2023-07-20 17:17:16 +0900

    Changed path of executable in job script

--- workloads/AMTE-evaluation/rigid2.sh
+++ workloads/AMTE-evaluation/rigid2.sh
@@ -4,6 +4,7 @@
 #MIN_NODES 2
 #MAX_NODES 2
 
-#cd $SCRIPT_DIR
-mpirun -n $NODES --hostfile $NODE_FILE /home/patrick/scheduler/scheduler/AMTE-evaluation/main
+
+cd ${SCRIPT_DIR}
+mpirun -n $NODES --hostfile $NODE_FILE ../../job_mpi/a.out
 
