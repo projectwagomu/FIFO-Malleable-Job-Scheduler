bb326ec5f0e73302d0627ea7091c36e9ec0c935b
Patrick Finnerty <finnerty.patrick@fine.cs.kobe-u.ac.jp>
2023-07-31 10:00:42 +0900

    Adjusted path for Middlelayer script

--- workloads/launchMiddlelayer.sh
+++ workloads/launchMiddlelayer.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-java -cp ../build/jar/Scheduler.jar middlelayer.Main
+
+cd ${CWD}/..
+pwd
+java -cp build/jar/Scheduler.jar middlelayer.Main
