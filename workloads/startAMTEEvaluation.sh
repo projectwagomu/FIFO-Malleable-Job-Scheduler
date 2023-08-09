169433fab532eea35313b4ed33cc4ca4f0446451
Jonas Posner <jonas.posner@uni-kassel.de>
2023-08-09 13:27:24 +0200

    scripts

--- workloads/startAMTEEvaluation.sh
+++ workloads/startAMTEEvaluation.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 
+CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd ${CWD}
+./launchScheduler.sh &
+sleep 1
+./launchMiddlelayer.sh &
+sleep 1
+AMTE-evaluation/submit-malleable.sh
